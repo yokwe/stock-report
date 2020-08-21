90c75e22eef49231aa170a45a76047a9f2b97166
hasegawa <hasegawa@ubuntu-2004.lan>
2020-08-21 14:55:44 +0900

    Use ScrapeUtil.

--- src/yokwe/stock/report/sbi/UpdateStockUS.java
+++ src/yokwe/stock/report/sbi/UpdateStockUS.java
@@ -3,14 +3,15 @@ package yokwe.stock.report.sbi;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import yokwe.UnexpectedException;
 import yokwe.util.CSVUtil;
+import yokwe.util.ScrapeUtil;
+import yokwe.util.StringUtil;
+import yokwe.util.StringUtil.MatcherFunction;
 import yokwe.util.http.HttpUtil;
 
 public class UpdateStockUS {
@@ -28,161 +29,190 @@ public class UpdateStockUS {
 		return CSVUtil.read(StockUS.class).file(PATH_SBI_US);
 	}
 	
-//	<tr>^M
-//	<th class="vaM alC">A</th>^M
-//	<td>Agilent Technologies<br>アジレント テクノロジーズ</td>^M
-//	<td>環境、食品の品質・安全性等の化学分析を行うツールを開発、提供</td>^M
-//	<td class="vaM alC">NYSE</td>^M
-//	</tr>^M
+// STOCK
+//	<tr>
+//	<th class="vaM alC">A</th>
+//	<td>Agilent Technologies<br>アジレント テクノロジーズ</td>
+//	<td>環境、食品の品質・安全性等の化学分析を行うツールを開発、提供</td>
+//	<td class="vaM alC">NYSE</td>
+//	</tr>
 
-//	<tr>^M
-//	<th class="vaM alC">ACWI</th>^M
-//	<td>iShares MSCI ACWI ETF<br>iシェアーズ MSCI ACWI ETF</td>^M
-//	<td class="vaM alC">NASDAQ</td>^M
-//	</tr>^M
+	public static class StockInfo {
+		public static final Pattern PAT = Pattern.compile(
+				"<tr>\\s+" +
+				"<th class=\"vaM alC\">(?<symbol>.+?)</th>\\s+" +
+				"<td>(?<nameEN>.+?)<br>(?<nameJP>.+?)</td>\\s+" +
+				"<td>(?<note>.+?)\\s*</td>\\s+" +
+				"<td class=\"vaM alC\">(?<exchange>.+?)</td>\\s+" +
+				"</tr>"
+		);
+		public static List<StockInfo> getInstance(String page) {
+			return ScrapeUtil.getList(StockInfo.class, PAT, page);
+		}
+		
+		public final String symbol;
+		public final String nameEN;
+		public final String nameJP;
+		public final String note;
+		public final String exchange;
+		
+		public StockInfo(String symbol, String nameEN, String nameJP, String note, String exchange) {
+			this.symbol   = symbol;
+			this.nameEN   = nameEN;
+			this.nameJP   = nameJP;
+			this.note     = note;
+			this.exchange = exchange;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("%s %s %s %s %s", symbol, nameEN, nameJP, note, exchange);
+		}
+	}
 
-	private static final String PAT_STRING_CAT = "<h3 class=\"ttlM\">(.+?)</h3>";
-	private static final String PAT_STRING_TAB = "<table class=\"md-l-table-01 md-l-utl-mt10\">";
-	
-	private static final String PAT_STRING_BEGIN  = "<tr>";
-	private static final String PAT_STRING_SYM = "<th class=\"vaM alC\">(.+?)</th>";
-	private static final String PAT_STRING_NAME = "<td>(.+?)<br>(.+?)</td>";
-	private static final String PAT_STRING_EXCH = "<td class=\"vaM alC\">(.+?)</td>";
-	private static final String PAT_STRING_END  = "</tr>";
+// ETF
+//	<tr>
+//	<th class="vaM alC">QQQ</th>
+//	<td>Invesco QQQ Trust,Series 1 ETF<br>インベスコ QQQ トラスト シリーズ1 ETF</td>
+//	<td class="vaM alC">NASDAQ</td>
+//	</tr>
+//
+//	<tr>
+//	<th class="vaM alC">FINX</th><td>Global X FinTech ETF<br>グローバルX フィンテック ETF</td>
+//	<td class="vaM alC">NASDAQ</td>
+//	</tr>
+//
+//	<tr>
+//	<th class="vaM alC">AIQ</th>
+//	<td>Global X Funds Global X Artificial Intelligence & Technology ETF<br>グローバルX AI＆ビッグデータETF
+//	</td>
+//	<td class="vaM alC">NASDAQ</td>
+//	</tr>    
+//
+//	<tr><th class="vaM alC">XLC</th>
+//	<td>Communication Service Select Sector SPDR Fund<br>コミュニケーション サービス セレクト セクターSPDRファンド</td>
+//	<td class="vaM alC">NYSE Arca</td>
+//	</tr>
+//	<tr>
+
+
+	public static class ETFInfo {
+		public static final Pattern PAT = Pattern.compile(
+				"<tr>\\s*" +
+				"<th class=\"vaM alC\">(?<symbol>.+?)</th>\\s*" +
+				"<td>(?<nameEN>.+?)<br>(?<nameJP>.+?)(\\s+)?</td>\\s+" +
+				"<td class=\"vaM alC\">(?<exchange>.+?)</td>\\s+" +
+				"</tr>"
+		);
+		public static List<ETFInfo> getInstance(String page) {
+			return ScrapeUtil.getList(ETFInfo.class, PAT, page);
+		}
+		
+		public final String symbol;
+		public final String nameEN;
+		public final String nameJP;
+		public final String exchange;
+		
+		public ETFInfo(String symbol, String nameEN, String nameJP, String exchange) {
+			this.symbol   = symbol;
+			this.nameEN   = nameEN;
+			this.nameJP   = nameJP;
+			this.exchange = exchange;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("%s %s %s %s", symbol, nameEN, nameJP, exchange);
+		}
+	}
+
+// ADR
+//	<tr>
+//	<th><div class="thM alC"><p class="fm01">BMA</p></div></th>
+//	<td><div class="tdM"><p class="fm01">Banco Macro SA<br>バンコ マクロ</p></div></td>
+//	<td><div class="tdM"><p class="fm01">アルゼンチンの金融機関</p></div></td>
+//	<td><div class="tdM alC"><p class="fm01">NYSE</p></div></td>
+//	</tr>
+
+//	<tr>
+//	<th><div class="thM alC"><p class="fm01">PKX</p></div></th>
+//	<td><div class="tdM"><p class="fm01">POSCO<BR>ポスコ</p></div></td>
+//	<td><div class="tdM"><p class="fm01">韓国の鉄鋼メーカー</p></div></td>
+//	<td><div class="tdM alC"><p class="fm01">NYSE</p></div></td>
+//	</tr>
+
+		public static class ADRInfo {
+			public static final Pattern PAT = Pattern.compile(
+					"<tr>\\s*" +
+					"<th><div class=\"thM alC\"><p class=\"fm01\">(?<symbol>.+?)</p></div></th>\\s+" +
+					"<td><div class=\"tdM\"><p class=\"fm01\">(?<nameEN>.+?)<(br|BR)>(?<nameJP>.+?)</p></div></td>\\s+" +
+					"<td><div class=\"tdM\"><p class=\"fm01\">(?<note>.+?)\\s*</p></div></td>\\s+" +
+					"<td><div class=\"tdM alC\"><p class=\"fm01\">(?<exchange>.+?)</p></div></td>\\s+" +
+					"</tr>"
+			);
+			public static List<ADRInfo> getInstance(String page) {
+				return ScrapeUtil.getList(ADRInfo.class, PAT, page);
+			}
 			
-	private static final Pattern PAT_CAT = Pattern.compile(PAT_STRING_CAT);
-	private static final Pattern PAT_TAB = Pattern.compile(PAT_STRING_TAB);
-	
-	private static final Pattern PAT_BEGIN = Pattern.compile(PAT_STRING_BEGIN);
-	private static final Pattern PAT_SYM = Pattern.compile(PAT_STRING_SYM);
-	private static final Pattern PAT_NAME = Pattern.compile(PAT_STRING_NAME);
-	private static final Pattern PAT_EXCH = Pattern.compile(PAT_STRING_EXCH);
-	private static final Pattern PAT_END = Pattern.compile(PAT_STRING_END);
-	
+			public final String symbol;
+			public final String nameEN;
+			public final String nameJP;
+			public final String note;
+			public final String exchange;
+			
+			public ADRInfo(String symbol, String nameEN, String nameJP, String note, String exchange) {
+				this.symbol   = symbol;
+				this.nameEN   = nameEN;
+				this.nameJP   = nameJP;
+				this.note     = note;
+				this.exchange = exchange;
+			}
+			
+			@Override
+			public String toString() {
+				return String.format("%s %s %s %s %s", symbol, nameEN, nameJP, note, exchange);
+			}
+		}
 
 	public static void main(String[] args) {
 		logger.info("START");
+
+		String string = HttpUtil.getInstance().withCharset(SOURCE_ENCODING).download(SOURCE_URL).result;
+				
+		// remove XML comment
+		Pattern PAT_COMMENT = Pattern.compile("<!--.*?-->", Pattern.MULTILINE | Pattern.DOTALL);
+		MatcherFunction<String> OP_REMOVE_COMMENT = (m) -> "";
+		String page = StringUtil.replace(string, PAT_COMMENT, OP_REMOVE_COMMENT);
+//		logger.info("string {}", string.length());
+//		logger.info("page   {}", page.length());
 		
-		Matcher mCat = PAT_CAT.matcher("");
-		Matcher mTable = PAT_TAB.matcher("");
-		
-		Matcher mBegin = PAT_BEGIN.matcher("");
-		Matcher mSym   = PAT_SYM.matcher("");
-		Matcher mName  = PAT_NAME.matcher("");
-		Matcher mExch  = PAT_EXCH.matcher("");
-		Matcher mEnd   = PAT_END.matcher("");
-		
+		List<StockInfo> stockInfoList = StockInfo.getInstance(page);
+		int countStock = stockInfoList.size();
+
+		List<ADRInfo> adrInfoList = ADRInfo.getInstance(page);
+		int countADR = adrInfoList.size();
+
+		List<ETFInfo> etfInfoList = ETFInfo.getInstance(page);
+		int countETF = etfInfoList.size();
+
 		List<StockUS> stockUSList = new ArrayList<>();
-		int countStock = 0;
-		int countETF = 0;
 		
-		String content = HttpUtil.getInstance().withCharset(SOURCE_ENCODING).download(SOURCE_URL).result;
-		String[] lines = content.split("[\r\n]+");
-		
-		String category = "???";
-		for(int i = 0; i < lines.length; i++) {
-			String line = lines[i].trim();
-			
-			{
-				mCat.reset(line);
-				if (mCat.find()) {
-					category = mCat.group(1);
-					category = category.replace("銘柄一覧", "");
-					category = category.replace("普通株式一覧", "STOCK");
-					logger.info("category {}", category);
-					continue;
-				}
-			}
-			
-			{
-				mTable.reset(line);
-				if (mTable.find()) {
-					for(;;) {
-						line = lines[++i];
-						if (line.equals("</table>")) {
-							break;
-						}
-						
-						mBegin.reset(line);
-						if (mBegin.find()) {
-							String line1;
-							if (line.startsWith("<tr><th")) {
-								line1 = line.substring(4);
-							} else {
-								line1 = lines[++i];
-							}
-							String line2;
-							if (line1.contains("</th><td>")) {
-								int pos = line1.indexOf("<td>");
-								line2 = line1.substring(pos);
-								line1 = line1.substring(0, pos);
-							} else {
-								line2 = lines[++i];
-							}
-							String line3 = lines[++i];
-							if (line3.startsWith("<td>")) line3 = lines[++i];
-							String line4 = lines[++i];
-							
-							if (line1.startsWith("<th style=\"width: 13%")) continue;
-							
-							mSym.reset (line1);
-							mName.reset(line2);
-							mExch.reset(line3);
-							mEnd.reset (line4);
-							
-							String symbol;
-							String name;
-							String exch;
-							
-							if (mSym.find()) {
-								symbol = mSym.group(1);
-							} else {
-								logger.error("Unexpected line  symbol  {}", line1);
-								throw new UnexpectedException("Unexpected line");
-							}
-							if (mName.find()) {
-								name = mName.group(1);
-							} else {
-								logger.error("Unexpected line  name  {}", line2);
-								throw new UnexpectedException("Unexpected line");
-							}
-							if (mExch.find()) {
-								exch = mExch.group(1);
-							} else {
-								logger.error("Unexpected line  exch  {}", line3);
-								throw new UnexpectedException("Unexpected line");
-							}
-							if (!mEnd.find()) {
-								logger.error("Unexpected line  end   {}", line4);
-								throw new UnexpectedException("Unexpected line");
-							}
-							//logger.info("{}  {}  {}  {}", symbol, name, exch);
-							
-							StockUS stockUS = new StockUS(symbol, name, exch, category);
-							stockUSList.add(stockUS);
-							
-							if (category.equals("STOCK")) countStock++;
-							else if (category.equals("ETF")) countETF++;
-							else {
-								logger.error("Unexpected category   {}", category);
-								throw new UnexpectedException("Unexpected category");
-							}
-						}
-					}
-				}
-			}
-		}
-		
+		// String ticker, String name, String exchange, String category
+		stockInfoList.stream().map(o -> new StockUS(o.symbol, o.nameEN, o.exchange, "STOCK")).forEach(o -> stockUSList.add(o));
+		adrInfoList.stream().map(o -> new StockUS(o.symbol, o.nameEN, o.exchange, "ADR")).forEach(o -> stockUSList.add(o));
+		etfInfoList.stream().map(o -> new StockUS(o.symbol, o.nameEN, o.exchange, "ETF")).forEach(o -> stockUSList.add(o));
+
 		Collections.sort(stockUSList);
 
 		logger.info("URL    = {}", SOURCE_URL);
 		logger.info("OUTPUT = {}", PATH_SBI_US);
 		save(stockUSList);
 
-		logger.info("ETF    = {}", String.format("%5d", countETF));
 		logger.info("STOCK  = {}", String.format("%5d", countStock));
-		logger.info("TOTAL  = {}", String.format("%5d", countETF + countStock));
-		
+		logger.info("ADR    = {}", String.format("%5d", countADR));
+		logger.info("ETF    = {}", String.format("%5d", countETF));
+		logger.info("TOTAL  = {}", String.format("%5d", countETF + countADR + countStock));
+
 		logger.info("STOP");		
 	}
 }
